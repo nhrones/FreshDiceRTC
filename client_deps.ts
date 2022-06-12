ee805873d6f7eb90aaa15cb0363336842e649ca6
Nick Hrones <nhrones@users.noreply.github.com>
2022-06-12 11:09:28 -0400

    Update client_deps.ts

--- client_deps.ts
+++ client_deps.ts
@@ -8,6 +8,6 @@ export {
     signal, 
     initialize,    
 } from "https://raw.githubusercontent.com/nhrones/Comms/master/mod.ts" 
+//} from  "file:///C:/Users/nhron/Documents/GitHub/WebRTC/New-Comms/mod.ts";
 
-/* local copy */
-// from  "file:///C:/Users/nhron/Documents/GitHub/Comms/mod.ts";
+//} from "https://raw.githubusercontent.com/nhrones/Comms/master/mod.ts" 
